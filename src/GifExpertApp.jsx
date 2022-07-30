fd123a924b8993204d05d504d85243f7cfea1853
Cuervolu <cuervolu@protonmail.com>
2022-07-29 22:58:47 -0400

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { AddCategory,GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['YugiOH']);
+
+  const onAddCategory = (newCategory) => {
+    if (
+      !categories.findIndex(
+        (category) => category.toLowerCase() === newCategory.toLowerCase()
+      )
+    )
+      return;
+    setCategories([newCategory, ...categories]);
+  };
+  return (
+    <>
+      <h1>Buscador de Gifs</h1>
+      <AddCategory onNewCategory={onAddCategory} />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
